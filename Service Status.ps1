05b8a5183139d09a55168e7592501b0c133b0767
Aygran <95911512+Aygran@users.noreply.github.com>
2023-01-31 10:58:10 -0700

    Service Status Script
    script that pulls all the current services on a server, showing who is assigned to run them, and their current status.

---
+++ Service Status.ps1
@@ -0,0 +1,14 @@
+<# 
+
+Created by: Mitchell Edwards
+Last Updated: 1/31/23
+
+Simple one liner that pulls all services on a server, filters out the service name, who is assigned to the service, and it's current status.
+
+Change the Export-CSV location in the quotation marks.
+
+#>
+
+Get-CIMInstance -Class Win32_Service | 
+    Select Name, StartName, State | 
+        Export-CSV "Change Me" -NoTypeInformation -Append
